bb8f3de10e3595149361dba09bf13bf7d61da6d2
devzons <devzons@gmail.com>
2019-02-12 13:47:58 -0800

    finished the static part

--- src/app/layout/App.js
+++ src/app/layout/App.js
@@ -1,13 +1,19 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import { Container } from 'semantic-ui-react'
+import EventDashboard from '../../features/event/EventDashboard/EventDashboard'
+import NavBar from '../../features/nav/NavBar/NavBar'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Ze-vents</h1>
-      </div>
+      <div>
+        <NavBar/>
+        <Container className="main">
+          <EventDashboard/>
+        </Container>
+      </div>      
     );
   }
 }
 
-export default App;
+export default App
